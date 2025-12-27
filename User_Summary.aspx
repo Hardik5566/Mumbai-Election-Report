ffd4ae4aebf809ce547431c033234cfe321284e1
VaghasiyaPrit <vaghasiyaprit799@gmail.com>
2025-12-27 15:09:11 +0530

    27-12 Prit

--- User_Summary.aspx
+++ User_Summary.aspx
@@ -19,13 +19,13 @@
             background-color: darkgreen !important;
         }
 
-         .bg_notactive {
+        .bg_notactive {
             background-color: darkred !important;
         }
 
-         .table td{
-             text-align:center !important;
-         }
+        .table td {
+            text-align: center !important;
+        }
     </style>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="body" runat="Server">
@@ -38,13 +38,26 @@
 
         <div class="ms-auto btn_header">
             <div class="btn-group">
-                <button type="button" class="btn btn-sm btn-danger btn_add" data-bs-toggle="modal" data-bs-target="#modal_add">Export</button>
+                <asp:Button ID="btn_export" class="btn btn-sm btn-danger btn_add" OnClick="btn_export_Click" runat="server" Text="Export" />
+               <%-- <button type="button" class="btn btn-sm btn-danger btn_add" data-bs-toggle="modal" data-bs-target="#modal_add">Export</button>--%>
             </div>
         </div>
     </div>
 
     <div class="card">
+        <div class="card-body">
+            <asp:DropDownList ID="ddl_district" OnSelectedIndexChanged="ddl_district_SelectedIndexChanged" runat="server" CssClass="form-control" AutoPostBack="true">
+                <asp:ListItem Value="">-- Select Zone --</asp:ListItem>
+                <asp:ListItem Value="1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 44, 45, 46, 47, 48, 49">North Mumbai</asp:ListItem>
+                <asp:ListItem Value="103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142">North East</asp:ListItem>
+                <asp:ListItem Value="70, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 149, 151, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171">North Central</asp:ListItem>
+                <asp:ListItem Value="37, 38, 39, 40, 41, 42, 43, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 121">North West</asp:ListItem>
+                <asp:ListItem Value="143, 144, 145, 146, 147, 148, 150, 152, 153, 154, 155, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 194, 200, 201">South Central</asp:ListItem>
+                <asp:ListItem Value="193, 195, 196, 197, 198, 199, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227">South Mumbai</asp:ListItem>
+            </asp:DropDownList>
 
+
+        </div>
         <div class="card-body">
             <div class="table-responsive">
                 <asp:GridView ID="grid_user_summary"
@@ -57,60 +70,60 @@
 
                     <Columns>
 
-                       
-                      <asp:TemplateField HeaderText="Ward" SortExpression="ward_no">
-            <ItemTemplate><%# Eval("ward_no") %></ItemTemplate>
-        </asp:TemplateField>
 
-        <asp:TemplateField HeaderText="Total User" SortExpression="total_user">
-            <ItemTemplate><%# Eval("total_user") %></ItemTemplate>
-        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Ward" SortExpression="ward_no">
+                            <ItemTemplate><%# Eval("ward_no") %></ItemTemplate>
+                        </asp:TemplateField>
 
-        <asp:TemplateField HeaderText="Active User" SortExpression="total_active_user">
-            <ItemStyle CssClass="bg_active text-white text-center" />
-            <ItemTemplate><%# Eval("total_active_user") %></ItemTemplate>
-        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Total User" SortExpression="total_user">
+                            <ItemTemplate><%# Eval("total_user") %></ItemTemplate>
+                        </asp:TemplateField>
 
-        <asp:TemplateField HeaderText="In-Active User" SortExpression="total_inactive_user">
-            <ItemStyle CssClass="bg_notactive text-white text-center" />
-            <ItemTemplate><%# Eval("total_inactive_user") %></ItemTemplate>
-        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Active User" SortExpression="total_active_user">
+                            <ItemStyle CssClass="bg_active text-white text-center" />
+                            <ItemTemplate><%# Eval("total_active_user") %></ItemTemplate>
+                        </asp:TemplateField>
 
-        <asp:TemplateField HeaderText="Admin" SortExpression="admin">
-            <ItemTemplate><%# Eval("admin") %></ItemTemplate>
-        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="In-Active User" SortExpression="total_inactive_user">
+                            <ItemStyle CssClass="bg_notactive text-white text-center" />
+                            <ItemTemplate><%# Eval("total_inactive_user") %></ItemTemplate>
+                        </asp:TemplateField>
 
-        <asp:TemplateField HeaderText="Sub Admin" SortExpression="sub_admin">
-            <ItemTemplate><%# Eval("sub_admin") %></ItemTemplate>
-        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Admin" SortExpression="admin">
+                            <ItemTemplate><%# Eval("admin") %></ItemTemplate>
+                        </asp:TemplateField>
 
-        <asp:TemplateField HeaderText="Sakti" SortExpression="sakti">
-            <ItemTemplate><%# Eval("sakti") %></ItemTemplate>
-        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Sub Admin" SortExpression="sub_admin">
+                            <ItemTemplate><%# Eval("sub_admin") %></ItemTemplate>
+                        </asp:TemplateField>
 
-        <asp:TemplateField HeaderText="Booth Pramukh" SortExpression="booth_pramukh">
-            <ItemTemplate><%# Eval("booth_pramukh") %></ItemTemplate>
-        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Sakti" SortExpression="sakti">
+                            <ItemTemplate><%# Eval("sakti") %></ItemTemplate>
+                        </asp:TemplateField>
 
-        <asp:TemplateField HeaderText="Sah Sakti" SortExpression="sah_sakti">
-            <ItemTemplate><%# Eval("sah_sakti") %></ItemTemplate>
-        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Booth Pramukh" SortExpression="booth_pramukh">
+                            <ItemTemplate><%# Eval("booth_pramukh") %></ItemTemplate>
+                        </asp:TemplateField>
 
-        <asp:TemplateField HeaderText="Sah Booth Pramukh" SortExpression="sah_booth_pramukh">
-            <ItemTemplate><%# Eval("sah_booth_pramukh") %></ItemTemplate>
-        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Sah Sakti" SortExpression="sah_sakti">
+                            <ItemTemplate><%# Eval("sah_sakti") %></ItemTemplate>
+                        </asp:TemplateField>
 
-        <asp:TemplateField HeaderText="Karykarta" SortExpression="karykarta">
-            <ItemTemplate><%# Eval("karykarta") %></ItemTemplate>
-        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Sah Booth Pramukh" SortExpression="sah_booth_pramukh">
+                            <ItemTemplate><%# Eval("sah_booth_pramukh") %></ItemTemplate>
+                        </asp:TemplateField>
 
-        <asp:TemplateField HeaderText="Phonebook User" SortExpression="phonebook_match_user">
-            <ItemTemplate><%# Eval("phonebook_match_user") %></ItemTemplate>
-        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Karykarta" SortExpression="karykarta">
+                            <ItemTemplate><%# Eval("karykarta") %></ItemTemplate>
+                        </asp:TemplateField>
 
-        <asp:TemplateField HeaderText="Phonebook Voter" SortExpression="phonebook_match_voter">
-            <ItemTemplate><%# Eval("phonebook_match_voter") %></ItemTemplate>
-        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Phonebook User" SortExpression="phonebook_match_user">
+                            <ItemTemplate><%# Eval("phonebook_match_user") %></ItemTemplate>
+                        </asp:TemplateField>
+
+                        <asp:TemplateField HeaderText="Phonebook Voter" SortExpression="phonebook_match_voter">
+                            <ItemTemplate><%# Eval("phonebook_match_voter") %></ItemTemplate>
+                        </asp:TemplateField>
 
 
                     </Columns>
@@ -119,7 +132,5 @@
             </div>
         </div>
     </div>
-
-
 </asp:Content>
 
--- User_Summary.aspx.cs
+++ User_Summary.aspx.cs
@@ -6,6 +6,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
 using System.IO;
+using ClosedXML.Excel;
 
 public partial class User_Summary : System.Web.UI.Page
 {
@@ -19,7 +20,6 @@ public partial class User_Summary : System.Web.UI.Page
     {
         try
         {
-
             if (!IsPostBack)
             {
                 ViewState["SortDirection"] = "ASC";
@@ -40,12 +40,26 @@ public partial class User_Summary : System.Web.UI.Page
         try
         {
             DataSet ds = BAL_Report.dis_user_summary("0");
-            if (ds.Tables.Count > 0)
+            if (ds.Tables.Count == 0) return;
+            ViewState["data"] = ds.Tables[0];
+
+            DataTable dt = ds.Tables[0];
+            if (!string.IsNullOrWhiteSpace(ddl_district.SelectedValue))
             {
-                grid_user_summary.DataSource = ds.Tables[0];
-                grid_user_summary.DataBind();
-                ViewState["data"] = ds.Tables[0];
+                var wardList = ddl_district.SelectedValue
+                    .Split(',')
+                    .Select(x => int.Parse(x.Trim()))
+                    .ToList();
+
+                var rows = dt.AsEnumerable()
+                             .Where(r => wardList.Contains(r.Field<int>("ward_no")));
+
+                dt = rows.Any() ? rows.CopyToDataTable() : dt.Clone();
             }
+            grid_user_summary.DataSource = dt;
+            grid_user_summary.DataBind();
+            ViewState["filtered_data"] = dt;
+
         }
         catch (Exception)
         {
@@ -66,7 +80,7 @@ public partial class User_Summary : System.Web.UI.Page
 
     private void BindGrid(string sortExpression = "", string sortDirection = "ASC")
     {
-        DataTable dt = (DataTable)ViewState["data"]; // from sp_get_user_summary
+        DataTable dt = (DataTable)ViewState["data"];
 
         if (!string.IsNullOrEmpty(sortExpression))
         {
@@ -77,5 +91,169 @@ public partial class User_Summary : System.Web.UI.Page
         grid_user_summary.DataSource = dt;
         grid_user_summary.DataBind();
     }
+    protected void btn_export_Click(object sender, EventArgs e)
+    {
+        if (ViewState["filtered_data"] != null)
+        {
+            DataTable dt = ViewState["filtered_data"] as DataTable;
 
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                DataTable exportDt = new DataTable();
+                exportDt.Columns.Add("Ward", typeof(string));
+                exportDt.Columns.Add("Total User", typeof(int));
+                exportDt.Columns.Add("Active User", typeof(int));
+                exportDt.Columns.Add("Inactive User", typeof(int));
+                exportDt.Columns.Add("Admin", typeof(int));
+                exportDt.Columns.Add("Sub Admin", typeof(int));
+                exportDt.Columns.Add("Sakti", typeof(int));
+                exportDt.Columns.Add("Booth Pramukh", typeof(int));
+                exportDt.Columns.Add("Sah Sakti", typeof(int));
+                exportDt.Columns.Add("Sah Booth Pramukh", typeof(int));
+                exportDt.Columns.Add("Karykarta", typeof(int));
+                exportDt.Columns.Add("Phonebook User", typeof(int));
+                exportDt.Columns.Add("Phonebook Voter", typeof(int));
+                foreach (DataRow row in dt.Rows)
+                {
+                    exportDt.Rows.Add(
+                        row["ward_no"], row["total_user"], row["total_active_user"], row["total_inactive_user"], row["admin"], row["sub_admin"], row["sakti"], row["booth_pramukh"], row["sah_sakti"], row["sah_booth_pramukh"], row["karykarta"], row["phonebook_match_user"], row["phonebook_match_voter"]
+                    );
+                }
+                DataRow totalRow = exportDt.NewRow();
+                totalRow["Ward"] = "Total";
+
+                foreach (DataColumn col in exportDt.Columns)
+                {
+                    if (col.ColumnName != "Ward")
+                    {
+                        int sum = 0;
+                        foreach (DataRow row in exportDt.Rows)
+                        {
+                            if (row[col] != DBNull.Value)
+                                sum += Convert.ToInt32(row[col]);
+                        }
+                        totalRow[col] = sum;
+                    }
+                }
+                exportDt.Rows.Add(totalRow);
+
+                using (var wb = new ClosedXML.Excel.XLWorkbook())
+                {
+                    var ws = wb.Worksheets.Add("FilteredData");
+                    int currentCol = 1;
+                    foreach (DataColumn col in exportDt.Columns)
+                    {
+                        var headerCell = ws.Cell(1, currentCol);
+                        headerCell.Value = col.ColumnName;
+                        headerCell.Style.Font.Bold = true;
+                        headerCell.Style.Font.FontColor = XLColor.Black;
+                        headerCell.Style.Fill.BackgroundColor = XLColor.White;
+                        headerCell.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                        headerCell.Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+                        headerCell.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                        if (col.ColumnName == "Active User")
+                        {
+                            headerCell.Style.Font.FontColor = XLColor.White;
+                            headerCell.Style.Fill.BackgroundColor = XLColor.FromHtml("#3cae4d"); // green
+                        }
+                        else if (col.ColumnName == "Inactive User")
+                        {
+                            headerCell.Style.Font.FontColor = XLColor.White;
+                            headerCell.Style.Fill.BackgroundColor = XLColor.FromHtml("#f11702"); // red
+                        }
+                        else if (col.ColumnName == "Phonebook User" || col.ColumnName == "Phonebook Voter")
+                        {
+                            headerCell.Style.Font.FontColor = XLColor.White;
+                            headerCell.Style.Fill.BackgroundColor = XLColor.FromHtml("#4f74c5"); // blue
+                        }
+                        else
+                            headerCell.Style.Fill.BackgroundColor = XLColor.FromHtml("#eeeeee");
+
+                        currentCol++;
+                    }
+                    int currentRow = 2;
+                    foreach (DataRow row in exportDt.Rows)
+                    {
+                        currentCol = 1;
+                        foreach (DataColumn col in exportDt.Columns)
+                        {
+                            var cell = ws.Cell(currentRow, currentCol);
+                            cell.Value = row[col.ColumnName];
+                            cell.Style.Font.FontName = "Calibri";
+                            cell.Style.Font.FontSize = 11;
+                            cell.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                            cell.Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+                            cell.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+                            if (row["Ward"].ToString() == "Total")
+                            {
+                                cell.Style.Fill.BackgroundColor = XLColor.Black;
+                                cell.Style.Font.FontColor = XLColor.White;
+                                cell.Style.Font.Bold = true;
+                            }
+                            else if (col.ColumnName == "Active User")
+                                cell.Style.Fill.BackgroundColor = XLColor.FromHtml("#e3efda");
+                            else if (col.ColumnName == "Inactive User")
+                                cell.Style.Fill.BackgroundColor = XLColor.FromHtml("#f9e4d6");
+                            else if (col.ColumnName == "Phonebook User" || col.ColumnName == "Phonebook Voter")
+                                cell.Style.Fill.BackgroundColor = XLColor.FromHtml("#dfebf7");
+                            else
+                                cell.Style.Fill.BackgroundColor = XLColor.White;
+
+                            currentCol++;
+                        }
+                        currentRow++;
+                    }
+                    ws.Columns().AdjustToContents();
+                    foreach (var col in ws.Columns())
+                    {
+                        if (col.Width < 10)
+                            col.Width = 10;
+                    }
+                    ws.Range(1, 1, 1, exportDt.Columns.Count).SetAutoFilter();
+                    Response.Clear();
+                    Response.Buffer = true;
+                    string date = DateTime.UtcNow.ToString("yyyyMMdd_HHmm");
+                    Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                    Response.AddHeader("content-disposition", "attachment;filename=Report_" + date + ".xlsx");
+                    using (var ms = new System.IO.MemoryStream())
+                    {
+                        wb.SaveAs(ms);
+                        ms.WriteTo(Response.OutputStream);
+                    }
+                    Response.Flush();
+                    HttpContext.Current.ApplicationInstance.CompleteRequest();
+                }
+            }
+        }
+
+
+    }
+
+    protected void ddl_district_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DataTable dt = ViewState["data"] as DataTable;
+        if (dt != null)
+        {
+            DataTable filteredDt;
+            if (!string.IsNullOrWhiteSpace(ddl_district.SelectedValue))
+            {
+                List<int> wardList = ddl_district.SelectedValue
+                    .Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x.Trim()))
+                    .ToList();
+                var filteredRows = dt.AsEnumerable()
+                                                   .Where(row => wardList.Contains(row.Field<int>("ward_no")));
+                filteredDt = filteredRows.Any()
+                                   ? filteredRows.CopyToDataTable()
+                                   : dt.Clone();
+            }
+            else
+            {
+                filteredDt = dt.Copy();
+            }
+            grid_user_summary.DataSource = filteredDt;
+            grid_user_summary.DataBind();
+            ViewState["filtered_data"] = filteredDt;
+        }
+    }
 }
